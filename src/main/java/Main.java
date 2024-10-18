c03e581b681c4b16b0b0249df7bb5ce4607850c9
Jacob Wise <jacob@wisefam.net>
2024-10-18 15:22:45 -0600

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,34 +2,37 @@ public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         
-        switch(args[0]){
+        String command = args[0];
+        int a, b;
+
+        switch (command) {
             case "add":
-                int a = Integer.parseInt(args[1]);
-                int b = Integer.parseInt(args[2]);
-                System.out.println(calculator.add(a,b));
+                a = Integer.parseInt(args[1]);
+                b = Integer.parseInt(args[2]);
+                System.out.println(calculator.add(a, b));
                 break;
             case "subtract":
-                int a = Integer.parseInt(args[1]);
-                int b = Integer.parseInt(args[2]);
-                System.out.println(calculator.subtract(a,b));
+                a = Integer.parseInt(args[1]);
+                b = Integer.parseInt(args[2]);
+                System.out.println(calculator.subtract(a, b));
                 break;
             case "multiply":
-                int a = Integer.parseInt(args[1]);
-                int b = Integer.parseInt(args[2]);
-                System.out.println(calculator.multiply(a,b));
+                a = Integer.parseInt(args[1]);
+                b = Integer.parseInt(args[2]);
+                System.out.println(calculator.multiply(a, b));
                 break;
             case "divide":
-                int a = Integer.parseInt(args[1]);
-                int b = Integer.parseInt(args[2]);
-                System.out.println(calculator.divide(a,b));
+                a = Integer.parseInt(args[1]);
+                b = Integer.parseInt(args[2]);
+                System.out.println(calculator.divide(a, b));
                 break;
             case "fibonacci":
-                int a = Integer.parseInt(args[1]);
-                System.out.println(calculator.fibonacciNumberFinder‎‎‎(a));
+                a = Integer.parseInt(args[1]);
+                System.out.println(calculator.fibonacciNumberFinder(a));
                 break;
             case "binary":
-                int a = Integer.parseInt(args[1]);
-                System.out.println(calculator.intToBinaryNumber‎(a));
+                a = Integer.parseInt(args[1]);
+                System.out.println(calculator.intToBinaryNumber(a));
                 break;
             default:
                 System.out.println("Invalid operation");
