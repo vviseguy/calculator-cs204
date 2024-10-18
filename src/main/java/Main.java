907054cac672d0e541668bd71c6ddb613fe8e828
Jacob Wise <jacob@wisefam.net>
2024-10-18 15:17:09 -0600

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testing Dockerfile");
+        Calculator calculator = new Calculator();
+        
+        switch(args[0]){
+            case "add":
+                int a = Integer.parseInt(args[1]);
+                int b = Integer.parseInt(args[2]);
+                System.out.println(calculator.add(a,b));
+                break;
+            case "subtract":
+                int a = Integer.parseInt(args[1]);
+                int b = Integer.parseInt(args[2]);
+                System.out.println(calculator.subtract(a,b));
+                break;
+            case "multiply":
+                int a = Integer.parseInt(args[1]);
+                int b = Integer.parseInt(args[2]);
+                System.out.println(calculator.multiply(a,b));
+                break;
+            case "divide":
+                int a = Integer.parseInt(args[1]);
+                int b = Integer.parseInt(args[2]);
+                System.out.println(calculator.divide(a,b));
+                break;
+            case "fibonacci":
+                int a = Integer.parseInt(args[1]);
+                System.out.println(calculator.fibonacciNumberFinder‎‎‎(a));
+                break;
+            case "binary":
+                int a = Integer.parseInt(args[1]);
+                System.out.println(calculator.intToBinaryNumber‎(a));
+                break;
+            default:
+                System.out.println("Invalid operation");
     }
 }
